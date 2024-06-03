349c42e7ff8852b3d37db308b11736a6f9fc9f78
Marcela Celani <104652600+marcela-celani@users.noreply.github.com>
2024-06-03 18:22:02 -0300

    Update Program.cs

--- web-api/Program.cs
+++ web-api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,13 +19,14 @@ builder.Services.AddSingleton<ItemsService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Comment out this line during development if causing issues
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
