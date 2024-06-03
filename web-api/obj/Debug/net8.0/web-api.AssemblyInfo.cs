7994736d0bcb3de5cb2ba46b5300eea792bf382b
marcela-celani <marcelacelani.ti@gmail.com>
2024-06-03 18:42:10 -0300

    ajustes deploy

--- web-api/obj/Debug/net8.0/web-api.AssemblyInfo.cs
+++ web-api/obj/Debug/net8.0/web-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d2f216597b6bbb4528e289baaa97b7f3ccac31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349c42e7ff8852b3d37db308b11736a6f9fc9f78")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
